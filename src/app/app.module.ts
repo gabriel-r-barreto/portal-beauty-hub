606dbd199e998ec6f286a882e2c42d4ffdf75a81
DanielTFontana <daniel.dtf@icloud.com>
2023-05-10 21:39:03 -0300

    inclusao do componente produtos e teste de funcao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { ProdutosComponent } from './produtos/produtos.component';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ProdutosComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/produtos/produtos.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produtos',
+  templateUrl: './produtos.component.html',
+  styleUrls: ['./produtos.component.scss']
+})
+export class ProdutosComponent {
+
+// let container = document.getElementById('container')
+
+// Obj.map((val) =>{
+//   container.innerHTML+=
+//   `<div class="produto-single">
+//   <img class="card-img-top" src="${val.img}"></img>
+//   <p>${val.nome}</p>
+//   <p class="valor">${val.valor}</p>
+//   <p>${val.descricao}</p>
+//   </div>`;
+// }) professor ve se vai funcionar :)
+
+}
